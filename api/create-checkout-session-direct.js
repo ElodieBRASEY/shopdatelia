2cc16b00dd7171ca61322cfee3415dac10a87a4d
ElodieBRASEY <elodie.brasey@inoco.fr>
2025-09-23 16:01:13 +0200

    Update create-checkout-session-direct.js

--- api/create-checkout-session-direct.js
+++ api/create-checkout-session-direct.js
@@ -7,29 +7,26 @@ export default async function handler(req, res) {
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2024-06-20' });
 
-  let email, team_size, pack;
+  let email, team_size, pack, promo_code;
 
   if (req.method === 'GET') {
     const q = req.query || {};
     email = q.email;
     team_size = q.team_size;
     pack = q.pack;
+    promo_code = q.promo_code;
   } else {
     const contentType = req.headers['content-type'] || '';
     if (contentType.includes('application/json')) {
-      ({ email, team_size, pack } = req.body || {});
+      ({ email, team_size, pack, promo_code } = req.body || {});
     } else if (contentType.includes('application/x-www-form-urlencoded')) {
       const bodyStr = await new Promise((resolve) => {
-        let data = '';
-        req.on('data', (c) => (data += c));
-        req.on('end', () => resolve(data));
+        let data = ''; req.on('data',(c)=>data+=c); req.on('end',()=>resolve(data));
       });
       const params = Object.fromEntries(new URLSearchParams(bodyStr));
-      email = params.email;
-      team_size = params.team_size;
-      pack = params.pack;
+      email = params.email; team_size = params.team_size; pack = params.pack; promo_code = params.promo_code;
     } else {
-      ({ email, team_size, pack } = req.body || {});
+      ({ email, team_size, pack, promo_code } = req.body || {});
     }
   }
 
@@ -45,8 +42,9 @@ export default async function handler(req, res) {
       payment_method_types: ['card'],
       metadata: {
         team_size: String(team),
-        pack: pack || ''
-      },
+        pack: (pack || '').toLowerCase(),
+        promo_code: promo_code || ''
+      }
     });
 
     res.writeHead(303, { Location: session.url });
